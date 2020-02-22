ee5b3fabb2e959e84f8271a4880d190395e49429
MitchelPaulin <mitchel0022@gmail.com>
2020-02-22 16:19:21 -0500

    Moved instance to US east

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,3 @@
-export IMAGE_FAMILY="pytorch-latest-gpu" # or "pytorch-latest-cpu" for non-GPU instances
-export ZONE="us-west1-b"
+export ZONE="us-east1-b"
 export INSTANCE_NAME="my-fastai-instance"
-export INSTANCE_TYPE="n1-highmem-8" # budget: "n1-highmem-4"
 gcloud compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
