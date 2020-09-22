ffe4e1cc39367f44357750c5744ca169832ce9a7
MitchelPaulin <mitchel0022@gmail.com>
2020-09-22 14:27:06 -0400

    Did some cleanup on the titanic model

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
 export ZONE="us-east1-b"
 export INSTANCE_NAME="my-fastai-instance"
 gcloud compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
+
+# sudo apt-get update && sudo apt-get install google-cloud-sdk
--- src/tabular_titanic_test.py
+++ src/tabular_titanic_test.py
@@ -7,21 +7,19 @@ import numpy as np
 # to get data set run ! kaggle competitions download -c titanic and move to /data/titanic
 
 train = pd.read_csv("../data/titanic/train.csv")
-# print(train.head()) # print out first 5 rows to verify data
 
 test = pd.read_csv("../data/titanic/test.csv")
-test.isnull().sum() #  replaces empty values with zeros (guess)
 
-test["Fare"] = test["Fare"].fillna(value = 0) # replace all NA values with zero
+test["Fare"] = test["Fare"].fillna(value=0)  # replace all NA values with zero
 
 # what we want to predict
 dep_var = 'Survived'
 
 # categorical variables, i.e. variables with discrete values
-cat_names = [ 'Sex', 'Ticket', 'Cabin', 'Embarked']
+cat_names = ['Sex', 'Ticket', 'Cabin', 'Embarked']
 
 # cont variables, i.e. variables with continous values
-cont_names = [ 'Age', 'SibSp', 'Parch', 'Fare']
+cont_names = ['Age', 'SibSp', 'Parch', 'Fare']
 
 # Transformations, pre processors
 # Categorify: What this does is it will analyze a column, and if it sees that a column is made up of discrete categories, it will assign an integer to each of those categories and replace what was originally in the column with an integer.
@@ -29,14 +27,15 @@ cont_names = [ 'Age', 'SibSp', 'Parch', 'Fare']
 # Normalize: Normalize the range of data
 procs = [FillMissing, Categorify, Normalize]
 
+# interpret the dep_var as a float type
 train[dep_var] = train[dep_var].astype(np.float32)
 
 # TODO
-to_nn = TabularDataLoaders.from_df(train, '../data/titanic/', procs=procs, cat_names=cat_names, cont_names=cont_names, y_names=dep_var, valid_idx=list(range(750, 891)), bs=32)
+to_nn = TabularDataLoaders.from_df(train, '../data/titanic/', procs=procs, cat_names=cat_names,
+                                   cont_names=cont_names, y_names=dep_var, valid_idx=list(range(750, 891)), bs=32)
 
 # Create the learner
 learn = tabular_learner(to_nn, n_out=1, loss_func=F.mse_loss)
 
 # Train for 15 epochs
 learn.fit_one_cycle(15, max_lr=slice(1e-03))
-
